d2e11155170d9816bcf04b357fae09a7483ffe14
John Nieves <karn09@gmail.com>
2014-06-23 21:18:39 -0400

    fixed bug where quiz to move to question outside of array length

--- app.js
+++ app.js
@@ -6,15 +6,15 @@
             choices: ['George Bush', 'Barack Obama', 'John F Kennedy', 'Bill Clinton'],
             answer: 1
         }, {
-            question: "Who is the current president of the USA?",
+            question: "Test2",
             choices: ['George Bush', 'Barack Obama', 'John F Kennedy', 'Bill Clinton'],
             answer: 1
         }, {
-            question: "Who is the current president of the USA?",
+            question: "Test3",
             choices: ['George Bush', 'Barack Obama', 'John F Kennedy', 'Bill Clinton'],
             answer: 1
         }, {
-            question: "Who is the current president of the USA?",
+            question: "Test4",
             choices: ['George Bush', 'Barack Obama', 'John F Kennedy', 'Bill Clinton'],
             answer: 1
         }];
@@ -23,46 +23,52 @@
         var questionSpan = document.getElementById('question');
         var scoreSpan = document.getElementById('score');
         var lists = document.querySelectorAll('li');
-        var choices = question.choices; // take passed in object and assign choices array
-
-        questionSpan.innerHTML = " " + question.question; // take passed in object and assign question 
-        for (var i = 0; i < 4; i++) {
-            lists[i].innerHTML = choices[i];
-        };
-        scoreSpan.innerHTML = " " + score;
+        if (currentQuestion < questions.length) {
+            var choices = question.choices; // take passed in object and assign choices array
+            questionSpan.innerHTML = " " + question.question; // take passed in object and assign question
+            for (var i = 0; i < 4; i++) {
+                lists[i].innerHTML = choices[i];
+            };
+            scoreSpan.innerHTML = " " + score;
+        }
     }
     var submitChoice = function submitChoice(choice) {
         var question = questions[currentQuestion];
-        if ((choice === question.answer) && (currentQuestion < questions.length)) {
+        if (choice === question.answer) {
             score += 20;
             currentQuestion += 1;
             addChoices(questions[currentQuestion]);
-        } else if (currentQuestion < questions.length) {
+        } else {
             currentQuestion += 1;
             addChoices(questions[currentQuestion]);
-        } else {
-            console.log("done");
         }
-        // call addChoices function to move to next question
-        /*if (currentQuestion < questions.length) {
-            addChoices(questions[currentQuestion]);
-        }*/
     }
 
     // event listeners 
     // can be refactored using loop and setting buttons in a container to make a nodelist
-    document.getElementById("A").addEventListener('click', function() {
-        submitChoice(0);
-    }, false);
-    document.getElementById("B").addEventListener('click', function() {
-        submitChoice(1);
-    }, false);
-    document.getElementById("C").addEventListener('click', function() {
-        submitChoice(2);
-    }, false);
-    document.getElementById("D").addEventListener('click', function() {
-        submitChoice(3);
-    }, false);
+
+    {
+        document.getElementById("A").addEventListener('click', function() {
+            if (currentQuestion < questions.length) {
+                submitChoice(0);
+            }
+        }, false);
+        document.getElementById("B").addEventListener('click', function() {
+            if (currentQuestion < questions.length) {
+                submitChoice(1);
+            }
+        }, false);
+        document.getElementById("C").addEventListener('click', function() {
+            if (currentQuestion < questions.length) {
+                submitChoice(2);
+            }
+        }, false);
+        document.getElementById("D").addEventListener('click', function() {
+            if (currentQuestion < questions.length) {
+                submitChoice(3);
+            }
+        }, false);
+    }
 
     // initialize first question in page
     addChoices(questions[currentQuestion]);
